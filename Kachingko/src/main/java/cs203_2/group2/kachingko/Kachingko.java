0fc9397790c6454e819bc98e4a73ad520e8bdc51
YordaSensei <nathsowner@gmail.com>
2025-07-14 11:15:35 +0800

    Kachingko Netbeans

---
+++ Kachingko/src/main/java/cs203_2/group2/kachingko/Kachingko.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package cs203_2.group2.kachingko;
+
+/**
+ *
+ * @author Admin
+ */
+public class Kachingko {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
